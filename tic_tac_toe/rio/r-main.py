cd942257e987cf52fae8e7c9196aaee5e911463e
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2024-08-20 09:12:03 -0700

    Update r-main ttt

--- tic_tac_toe/rio/r-main.py
+++ tic_tac_toe/rio/r-main.py
@@ -7,40 +7,35 @@
 import tkinter as tk
 from tkinter import ttk
 
-# class Tiles():
-#       def __init__(self) -> None:
-#             self.
-    
-def savePosn(event):
-  global lastx, lasty
-  lastx, lasty = event.x, event.y
+# class Tiles(tk.Canvas):
+#   def __init__(self, shape) -> None:
+#     super().__init__()
+#     self.shape = shape
 
-def addLine(event):
-  game_board.create_line((lastx, lasty, event.x, event.y))
-  savePosn(event)
-  
-    
+#     tile = tk.Canvas
+
+#   def draw(self):
+
+
 root = tk.Tk()
 root.title("Tic Tac Toe")
+root.resizable()
 
 frame = ttk.Frame(root)
 frame.grid()
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+game_board = tk.Canvas()
+
+# Title
 title = "TIC TAC TOE"
 font = ("Verdana", 24)
 title_label = tk.Label(frame, text=title, font=font)
 
-# for i in range(9):
-#       new Tiles
+game_board.create_rectangle(1, 1, 100, 100, fill="white")
 
-game_board = tk.Canvas(root)
-game_board.grid(column=0, row=1)
-
-game_board.bind("<Button-1>", savePosn)
-game_board.bind("<B1-Motion>", addLine)
-
+game_board.grid()
 title_label.grid()
 
 root.mainloop()
